41a40aaa2bdf648bf7e38e10e1e1d7414e2fe75c
Yuriy Lavrushin <yuriy.lavroushin@gmail.com>
2023-02-26 18:05:50 +0400

    feat: solution ready, pass 100% of local tests.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+
+    minValue;
+
+    maxValue;
+
+    currentValue;
+
+    constructor() {
+        this.minValue = null;
+        this.maxValue = null;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.currentValue = Math.round((this.maxValue - this.minValue) / 2 + this.minValue);
+        return this.currentValue;
     }
 
     lower() {
-
+        this.maxValue = this.currentValue;
+        return this.guess();
     }
 
     greater() {
-
+        this.minValue = this.currentValue;
+        return this.guess();
     }
 }
 
